ae189251ed36ec9c154a5cd6fc2d5c1b3a857517
= <=>
2023-02-12 00:29:13 +0700

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'line_connect';
+  ngOnInit(){
+    
+console.log('deuk')
+  }
+  open(){
+    alert('Please OPEN in APPLICATION :)');
+  }
+  download(){
+    let appURL ='';
+    const userAgent = window.navigator.userAgent;
+    if(userAgent.match(/android/)){
+      appURL = 'https://play.google.com/store/apps/details?id=jp.naver.line.android';
+    }
+    else if(userAgent.match(/iPhone|ipad|MAC/)){
+      appURL='https://apps.apple.com/app/line/id443904275'
+
+    }
+    else{
+      alert('Please download app on your smartphone');
+    }
+    window.location.href = appURL;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
